e210abf4c83e90ea93919be0a2ebf9ab0856f79a
mattiapomelli <mattiapomelli@gmail.com>
2023-04-25 16:49:27 +0300

    Add link to github in footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 
 import { Container } from "@components/layout/container";
+import GithubIcon from "@icons/github.svg";
 
 export const Footer = () => {
   return (
     <footer>
       <Container>
-        <div className="flex flex-wrap items-center justify-between gap-4 border-t border-base-content/10 py-6 text-sm">
+        <div className="flex flex-wrap items-center justify-between gap-4 border-t border-base-content/10 py-10">
           <div className="shrink-0 md:flex-1">© 2023, KnowledgeLayer</div>
 
-          {/* <p className="order-1 basis-full text-center md:order-none md:basis-auto">
-            KnowledgeLayer
-          </p> */}
-
-          <div className="flex items-center justify-end md:flex-1">Contact</div>
+          <div className="flex items-center justify-end md:flex-1">
+            <a
+              href="https://github.com/knowledgelayer/knowledgelayer-contracts"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GithubIcon className="h-6 w-6" />
+            </a>
+          </div>
         </div>
       </Container>
     </footer>
