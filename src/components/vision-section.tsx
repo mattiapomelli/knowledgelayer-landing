06ce36c70192ae03440a74a8ae41e00bbb351dab
mattiapomelli <mattiapomelli@gmail.com>
2023-04-26 14:31:33 +0300

    Change network image

--- src/components/vision-section.tsx
+++ src/components/vision-section.tsx
@@ -7,14 +7,18 @@ export const VisionSection = () => {
   return (
     <section className="bg-accent text-base-content">
       <Container className="flex flex-col items-center gap-10 py-20 md:flex-row">
-        <div className="order-1 flex flex-col gap-6 md:order-none">
+        <div className="order-1 flex flex-col gap-6 text-center md:order-none md:text-left">
           <p className="max-w-[34rem] text-3xl">
-            A network of people who want to expand and share their
+            A network of people who want to
+            <b> expand </b>
+            and
+            <b> share </b>
+            their
             <b> knowledge</b>
           </p>
           <p className="max-w-[28rem] text-lg text-base-content-neutral">
-            A network of learners, teachers and educational content that any
-            application can plug into.
+            KnowledgeLayer provides a shared pool of learners, teachers and
+            educational content that any application can plug into.
           </p>
           <a
             href="https://tiapome.notion.site/KnowledgeLayer-Manifesto-dc10fb32bfb34847a591894479f3107a"
