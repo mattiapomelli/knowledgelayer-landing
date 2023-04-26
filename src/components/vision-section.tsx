addd1bc29656ccba7508791c07a836d3c73844cd
mattiapomelli <mattiapomelli@gmail.com>
2023-04-26 13:35:14 +0300

    Add image in vision section

--- src/components/vision-section.tsx
+++ src/components/vision-section.tsx
@@ -1,21 +1,37 @@
+import Image from "next/image";
+
 import { Button } from "./basic/button";
 import { Container } from "./layout/container";
 
 export const VisionSection = () => {
   return (
     <section className="bg-accent text-base-content">
-      <Container className="flex flex-col items-center gap-10 py-20 text-center">
-        <p className="max-w-[34rem] text-3xl">
-          An ecosystem for people who want to expand and share their
-          <b> knowledge</b>
-        </p>
-        <a
-          href="https://tiapome.notion.site/KnowledgeLayer-Manifesto-dc10fb32bfb34847a591894479f3107a"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Button>Read the manifesto</Button>
-        </a>
+      <Container className="flex flex-col items-center gap-10 py-20 md:flex-row">
+        <div className="order-1 flex flex-col gap-6 md:order-none">
+          <p className="max-w-[34rem] text-3xl">
+            A network of people who want to expand and share their
+            <b> knowledge</b>
+          </p>
+          <p className="max-w-[28rem] text-lg text-base-content-neutral">
+            A network of learners, teachers and educational content that any
+            application can plug into.
+          </p>
+          <a
+            href="https://tiapome.notion.site/KnowledgeLayer-Manifesto-dc10fb32bfb34847a591894479f3107a"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button>Read the manifesto</Button>
+          </a>
+        </div>
+        <div>
+          <Image
+            src="/network.svg"
+            width="546px"
+            height="450px"
+            alt="Network of learners and teachers"
+          />
+        </div>
       </Container>
     </section>
   );
