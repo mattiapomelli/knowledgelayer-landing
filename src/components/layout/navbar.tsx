17571db45040509001c044f92188e4b3a7c6301c
mattiapomelli <mattiapomelli@gmail.com>
2023-04-26 14:31:43 +0300

    Adjust gap in navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -7,9 +7,9 @@ import { Container } from "./container";
 export const Navbar = () => {
   return (
     <header className="flex h-20 items-center">
-      <Container className="flex w-full items-center justify-between">
+      <Container className="flex w-full items-center justify-between gap-4">
         <Logo className="hidden md:block" />
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2 md:gap-4">
           <ThemeToggle />
           <a
             href="https://tiapome.notion.site/KnowledgeLayer-Manifesto-dc10fb32bfb34847a591894479f3107a"
