b46934aa5d6e70974533238b143de376c46c2612
mattiapomelli <mattiapomelli@gmail.com>
2023-04-26 13:35:03 +0300

    Refactor features section

--- src/components/features-section.tsx
+++ src/components/features-section.tsx
@@ -6,6 +6,9 @@ import {
 } from "@heroicons/react/24/outline";
 import { ReactNode } from "react";
 
+import ArrowDownGraphic from "@icons/arrow-down.svg";
+import ExplosionGraphic from "@icons/explosion.svg";
+
 import { Container } from "./layout/container";
 
 interface FeatureCardProps {
@@ -28,13 +31,13 @@ const FeatureCard = ({ title, description, icon }: FeatureCardProps) => {
 
 export const FeaturesSection = () => {
   return (
-    <Container className="flex flex-col items-center gap-10 py-20">
+    <Container className="flex flex-col items-center gap-10 pt-20 pb-32">
       <h3 className="max-w-[30rem] text-center text-3xl font-bold">
-        The key components of educational platforms
+        The key components for educational platforms
       </h3>
       <p className="max-w-[28rem] text-center text-lg text-base-content-neutral">
-        KnowledgeLayer provides the main building blocks so that you can focus
-        on building what makes your platform stand out
+        Avoid building your backend from scratch and focus on what makes your
+        edtech platform stand out.
       </p>
       <div className="grid auto-rows-fr grid-cols-1 gap-4 sm:grid-cols-2">
         <FeatureCard
@@ -45,18 +48,28 @@ export const FeaturesSection = () => {
         <FeatureCard
           icon={<CurrencyDollarIcon />}
           title="Escrow and dispute resolution"
-          description="Secure payments and resolve disputes between buyers and sellers"
-        />
-        <FeatureCard
-          icon={<StarIcon />}
-          title="Reviews and reputation"
-          description="Reviews for courses and teachers aggregated in a unified reputation across platforms"
+          description="Securely handle payments between students and teachers, and solve possible disputes"
         />
         <FeatureCard
           icon={<CheckBadgeIcon />}
           title="Verifiable credentials"
           description="Issue certificates and badges for the completion of courses to prove students' skills"
         />
+        <FeatureCard
+          icon={<StarIcon />}
+          title="Reviews and reputation"
+          description="Reviews for courses and teachers aggregated in a unified reputation across platforms"
+        />
+      </div>
+      <ArrowDownGraphic className="my-6 h-32 w-32" />
+      <div className="relative">
+        <p className="max-w-[34rem] text-center text-3xl">
+          Reduce development costs and time, and go to market
+          <b> faster</b>
+        </p>
+        <ExplosionGraphic className="absolute -left-8 h-14 w-14" />
+        <ExplosionGraphic className="absolute -right-10 top-10 h-12 w-12 rotate-[140deg]" />
+        <ExplosionGraphic className="absolute -top-16 left-40 h-12 w-12 rotate-12" />
       </div>
     </Container>
   );
