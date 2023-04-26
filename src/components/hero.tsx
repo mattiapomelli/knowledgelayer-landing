a59ba80b674657d950a7d1f2eedfe737feca79d5
mattiapomelli <mattiapomelli@gmail.com>
2023-04-26 14:31:51 +0300

    Add graphics to hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,11 @@
+import ExplosionGraphic from "@icons/explosion.svg";
+
 import { Button } from "./basic/button";
 import { Container } from "./layout/container";
 
 export const Hero = () => {
   return (
-    <Container className="flex flex-col items-center gap-6 py-28 text-center">
+    <Container className="relative flex flex-col items-center gap-6 overflow-hidden py-36 text-center">
       <h1 className="max-w-[32rem] text-4xl font-bold">
         The toolkit for building web3 educational platforms
       </h1>
@@ -18,6 +20,9 @@ export const Hero = () => {
       >
         <Button>Read the manifesto</Button>
       </a>
+      <ExplosionGraphic className="absolute left-[10%] bottom-[10%] h-14 w-14 md:bottom-[20%]" />
+      <ExplosionGraphic className="absolute right-[-20px] top-[35%] h-12 w-12 rotate-[140deg] md:right-[8%]" />
+      <ExplosionGraphic className="absolute top-[0%] left-[40%] h-12 w-12 rotate-12" />
     </Container>
   );
 };
