96fa7df43c0ca49c1eb891be07bafe0def7f2b17
mattiapomelli <mattiapomelli@gmail.com>
2023-04-27 20:27:46 +0300

    Swap theme toggle and navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -9,17 +9,18 @@ export const Navbar = () => {
     <header className="flex h-20 items-center">
       <Container className="flex w-full items-center justify-between gap-4">
         <Logo className="hidden md:block" />
-        <div className="flex items-center gap-2 md:gap-4">
+        <div className="flex items-center gap-3 md:gap-4">
+          <ThemeToggle />
           <nav>
             <a
               href="https://github.com/knowledgelayer/knowledgelayer-contracts"
               target="_blank"
               rel="noreferrer"
+              className="hover:text-accent-focus"
             >
               GitHub
             </a>
           </nav>
-          <ThemeToggle />
           <a
             href="https://tiapome.notion.site/KnowledgeLayer-Manifesto-dc10fb32bfb34847a591894479f3107a"
             target="_blank"
