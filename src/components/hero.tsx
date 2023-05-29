4afd0dd574684f7da4d14da49018a70c99f99279
mattiapomelli <mattiapomelli@gmail.com>
2023-05-29 16:53:03 -0700

    Remove comments

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,31 +1,10 @@
-// import { useState } from "react";
-// import { useForm } from "react-hook-form";
-
-// import { useSignup } from "@hooks/use-signup";
 import { DOCS_URL } from "@constants/urls";
 import ExplosionGraphic from "@icons/explosion.svg";
 
 import { Button } from "./basic/button";
-// import { Input } from "./basic/input";
 import { Container } from "./layout/container";
 
 export const Hero = () => {
-  // const { signup, isLoading } = useSignup();
-  // const [isSuccess, setIsSuccess] = useState(false);
-
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   formState: { errors },
-  //   reset,
-  // } = useForm<{ email: string }>();
-
-  // const onSubmit = handleSubmit(async (data) => {
-  //   await signup(data.email);
-  //   reset();
-  //   setIsSuccess(true);
-  // });
-
   return (
     <section className="relative">
       <Container className="relative flex flex-col items-center gap-6 py-36 text-center">
@@ -35,34 +14,7 @@ export const Hero = () => {
         <p className="max-w-[18rem] text-lg text-base-content-neutral sm:max-w-[26rem]">
           Build on a solid foundation and focus on what matters for your edtech
           platform.
-          {/* Get a step-by-step integration guide and start building on a solid
-          foundation. */}
         </p>
-
-        {/* <div className="flex flex-col items-center gap-4">
-          <form
-            onSubmit={onSubmit}
-            className="flex flex-col items-end gap-2 sm:flex-row"
-          >
-            <Input
-              label="Get the integration guide"
-              placeholder="Email"
-              type="email"
-              className="sm:min-w-[300px]"
-              block
-              {...register("email", { required: "Email is required" })}
-              error={errors.email?.message}
-            />
-            <Button loading={isLoading} disabled={isLoading}>
-              Start building
-            </Button>
-          </form>
-          {isSuccess && (
-            <div className="rounded-box w-full border-2 border-primary-content bg-success px-4 py-2 text-primary-content drop-shadow-default">
-              Will get back to you shortly! Check your email in a while 🎉
-            </div>
-          )}
-        </div> */}
 
         <a href={DOCS_URL} target="_blank" rel="noreferrer">
           <Button>Start building</Button>
