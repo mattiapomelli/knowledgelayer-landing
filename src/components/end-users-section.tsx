9c092af68663153334f7066158634a10e9a1180c
mattiapomelli <mattiapomelli@gmail.com>
2023-05-31 14:38:12 -0700

    Align card text to left

--- src/components/end-users-section.tsx
+++ src/components/end-users-section.tsx
@@ -6,11 +6,11 @@ import { Container } from "./layout/container";
 export const EndUsersSection = () => {
   return (
     <section>
-      <Container className="flex flex-col items-center gap-8 py-32 text-center">
+      <Container className="flex flex-col items-center gap-8 py-32">
         <h3 className="max-w-[30rem] text-center text-3xl font-bold">
           One profile, many platforms
         </h3>
-        <p className="max-w-[36rem] text-lg text-base-content-neutral">
+        <p className="max-w-[36rem] text-center text-lg text-base-content-neutral">
           With KnowledgeLayer, you have complete ownerhip of your profile,
           content and data. Forget about creating a new profile and starting
           from scratch every time you use a new platform.
@@ -23,7 +23,7 @@ export const EndUsersSection = () => {
           reviews and reputation follow you everywhere."
             title="For educators"
             icon={<StarIcon />}
-            className="flex-1 items-center"
+            className="flex-1"
           />
           <Card
             description="With KnowledgeLayer, you can access any platform in the network with a
@@ -32,7 +32,7 @@ export const EndUsersSection = () => {
         single place."
             title="For learners"
             icon={<AcademicCapIcon />}
-            className="flex-1 items-center"
+            className="flex-1"
           />
         </div>
       </Container>
