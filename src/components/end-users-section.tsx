2d7fb9391a3250fb3254c1f5c92402c6f6b1bf62
mattiapomelli <mattiapomelli@gmail.com>
2023-05-31 13:16:46 -0700

    Add icon in end users section card

--- src/components/end-users-section.tsx
+++ src/components/end-users-section.tsx
@@ -1,19 +1,30 @@
+import { AcademicCapIcon, StarIcon } from "@heroicons/react/24/outline";
 import cx from "classnames";
 import { ReactNode } from "react";
 
 import { TabItem, Tabs } from "./basic/tabs";
 import { Container } from "./layout/container";
 
-const TabCard = ({ children }: { children: ReactNode }) => {
+interface ComponentCardProps {
+  title: string;
+  description: string;
+  icon: ReactNode;
+}
+
+const TabCard = ({ title, description, icon }: ComponentCardProps) => {
   return (
     <div
       className={cx(
-        "rounded-box flex flex-col justify-center gap-4 bg-base-100 p-6 text-lg text-base-content-neutral",
+        "rounded-box flex flex-col items-center justify-center gap-4 bg-base-100 p-6 text-lg text-base-content-neutral",
         "border-2 border-base-content drop-shadow-default",
-        "min-h-[18rem] sm:min-h-[12rem]",
+        "min-h-[24rem] sm:min-h-[22rem] md:min-h-[20rem]",
       )}
     >
-      {children}
+      <div className="flex h-14 w-14 items-center justify-center rounded-full border-2 border-primary-content bg-primary drop-shadow-default">
+        <span className="h-8 w-8 text-primary-content">{icon}</span>
+      </div>
+      <h4 className="text-xl font-bold text-base-content">{title}</h4>
+      <p>{description}</p>
     </div>
   );
 };
@@ -23,23 +34,27 @@ export const EndUsersSection = () => {
     {
       label: "I'm an Educator",
       content: (
-        <TabCard>
-          With KnowledgeLayer, your content gets automatically distributed on
-          any platform of the network. This means that you are able to reach a
-          wider audience and monetize your content more effectively. Your
-          reviews and reputation follow you everywhere.
-        </TabCard>
+        <TabCard
+          description="With KnowledgeLayer, your content gets automatically distributed on
+        any platform of the network. This means that you are able to reach a
+        wider audience and monetize your content more effectively. Your
+        reviews and reputation follow you everywhere."
+          title="For educators"
+          icon={<StarIcon />}
+        />
       ),
     },
     {
-      label: "I'm a Student",
+      label: "I'm a Learner",
       content: (
-        <TabCard>
-          With KnowledgeLayer, you can access any platform in the network with a
-          single profile, to learn in multiple topic areas. All your
-          achievements, credentials and learning activity are aggregated in a
-          single place.
-        </TabCard>
+        <TabCard
+          description="With KnowledgeLayer, you can access any platform in the network with a
+        single profile, to learn in multiple topic areas. All your
+        achievements, credentials and learning activity are aggregated in a
+        single place."
+          title="For learners"
+          icon={<AcademicCapIcon />}
+        />
       ),
     },
   ];
